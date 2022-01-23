fba8a50b67d7987eb0d090ff7436c2f67ec1b0aa
Juan Jose <juancasz0208@gmail.com>
2022-01-23 17:35:32 -0500

    exercise 9.19 solved

--- part9/patientor/patientor_backend/models/types.ts
+++ part9/patientor/patientor_backend/models/types.ts
@@ -5,15 +5,14 @@ export interface Diagnose{
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
+//export interface Entry {}
 
 export interface Patient{
     id:string,
     name:string,
     dateOfBirth:string,
     ssn:string,
-    gender:string,
+    gender:Gender,
     occupation:string
     entries: Entry[]
 }
@@ -27,4 +26,50 @@ export enum Gender {
     Female = 'female',
     Other = 'other'
 }
+
+export interface BaseEntry {
+    id:string,
+    description:string,
+    date:string,
+    specialist:string,
+    diagnosisCodes?:Array<Diagnose['code']>
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export interface Discharge {
+    date:string;
+    criteria: string
+}
+
+export interface HospitalEntry extends BaseEntry{
+    type:"Hospital";
+    discharge: Discharge;
+}
+
+export interface SickLeave{
+    startDate: string;
+    endDate: string
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry{
+    type:"OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
