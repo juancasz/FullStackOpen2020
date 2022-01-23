a4399be97ee762068d28326331c5d96f246bed4c
Juan Jose <juancasz0208@gmail.com>
2022-01-23 18:11:44 -0500

    exercise 9.20 solved

--- part9/patientor/patientor-main/src/PatientInfoPage/index.tsx
+++ part9/patientor/patientor-main/src/PatientInfoPage/index.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { apiBaseUrl } from "../constants";
 import { addPatient, useStateValue } from "../state";
 import { useParams } from "react-router-dom";
-import { Patient } from "../types";
+import { Entry, Patient } from "../types";
 import { Icon } from "semantic-ui-react";
 import { Gender } from "../types";
 
 const PatientInfoPage = () => {
     const [{ patients }, dispatch] = useStateValue();
     const { id } = useParams<{ id: string }>();
+    const[patient,setPatient] = React.useState<Patient|undefined>(undefined);
 
     React.useEffect(() => {
         const getPatient = async (id:string) => {
@@ -17,6 +18,7 @@ const PatientInfoPage = () => {
                 const{data: patientFromApi} = await axios.get<Patient>(
                     `${apiBaseUrl}/patients/${id}`
                 );
+                setPatient(patientFromApi);
                 dispatch(addPatient(patientFromApi));
             }catch(e){
                 console.error(e);
@@ -29,32 +31,59 @@ const PatientInfoPage = () => {
             void getPatient(id);
         }else if(patient.ssn === undefined){
             void getPatient(id);
+        }else{
+            setPatient(patient);
         }
     },[dispatch]);
     
-    if(!patients[id]){
+    if(!patient){
         return(
-            <div>
-                Not Found
-            </div>
+            <div></div>
         );
     }
+
+    const Entries = () => {
+       if(patient.entries){
+           return(
+               <div>
+                    <h3>entries</h3>
+                    {patient.entries.map((entry:Entry) => (
+                        <div key={entry.id}>
+                            <p>{entry.date} {entry.description}</p>
+                            <ul>
+                                {entry.diagnosisCodes?.map((code) => (
+                                    <li key={code}>{code}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+               </div>
+           );
+       }
+
+       return(
+        <div></div>
+       );
+    };
 
     return(
         <div>
             <h2>
-                {patients[id].name} <Icon 
+                {patient.name} <Icon 
                     name={
-                        patients[id].gender === Gender.Male?
+                        patient.gender === Gender.Male?
                             "mars":
-                            patients[id].gender === Gender.Female?
+                            patient.gender === Gender.Female?
                                 "venus":
                                 "venus mars"
                     }
                 />
             </h2>
-            <h3>ssn: {patients[id].ssn}</h3>
-            <h3>occupation: {patients[id].occupation}</h3>
+            <h3>ssn: {patient.ssn}</h3>
+            <h3>occupation: {patient.occupation}</h3>
+            <br/>
+            <br/>
+            <Entries/>
         </div>
     );
 };
